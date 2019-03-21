7f0eb6367e52a079c26d21e31f84ac2ffaeed37d
Will Eccles <ghostyx123@gmail.com>
2019-03-21 13:09:59 -0400

    It works now, time to make it work better

--- src/multilingual.h
+++ src/multilingual.h
@@ -1,25 +1,35 @@
 #ifndef LIB_MULTILINGUAL_H
 #define LIB_MULTILINGUAL_H
 
-#include <map>
-#include <string>
 #include <fstream>
-#include <stdexcept>
+#include <map>
 #include <sstream>
+#include <stdexcept>
+#include <string>
+#include <vector>
 
-typedef lm_lang_id	std::string;	// language type
-typedef lm_dir		std::string;	// directory type
+typedef std::string	lm_lang_id;	// language type
+typedef std::string	lm_dir;	// directory type
 
 #ifdef LIB_MULTI_WSTRING // force use of std::wstring instead of std::string
-typedef lm_key		std::wstring;	// string dictionary key type
-typedef lm_val		std::wstring;	// string dictionary value type
+typedef std::wstring	lm_str;
 #else
-typedef lm_key		std::string;
-typedef lm_val		std::string;
+typedef std::string		lm_str;
 #endif
 
-typedef lm_listener_array std::map<lm_val&, lm_key>; // map refs to strings with their values
-// this would be used if you want your strings to be updated automagically
+typedef lm_str		lm_key;	// string dictionary key type
+typedef lm_str		lm_val;	// string dictionary value type
+
+typedef std::map<lm_key, lm_val>	lm_dict;
+
+namespace multilingual {
+	struct Listener {
+		lm_val	&str; // the string to update
+		lm_key	key; // the key to use to update the string
+	};
+};
+
+typedef std::vector<multilingual::Listener>	lm_listener_array;
 
 namespace multilingual {
 	class Translator {
@@ -28,10 +38,9 @@ namespace multilingual {
 		lm_lang_id	_fallback = "en_US";
 		lm_lang_id	_lang = "en_US";
 
-		std::map<lm_key, lm_val> _strvals;
-		std::map<lm_key, lm_val> _fallbackvals; // en_US by default
+		lm_dict _strvals;
 
-		lm_listener_array& _listeners; // update listeners
+		lm_listener_array _listeners; // update listeners
 
 		bool _updatelisteners = false;
 
@@ -43,43 +52,75 @@ namespace multilingual {
 		Translator(lm_lang_id lang, lm_lang_id fallback, lm_dir dir): _lang(lang), _fallback(fallback), _dir(dir) {};
 
 		// load languages files (.lang) in the directory
-		// returns the number of key/value pairs found
-		// if update listeners are enabled, this will update them accordingly
+		// - returns the number of key/value pairs found
+		// - if update listeners are enabled, this will update them accordingly
+		// - throws std::runtime_error if neither the language file nor the fallback
+		// could be loaded.
 		unsigned int load();
 
+		// set a new directory
+		void setdir(lm_dir newdir);
+
+		// set a new fallback language
+		void setfallback(lm_lang_id newlang);
+
 		// change which language to load
-		// this does not load the file!
+		// - this does not load the file!
 		void setlang(lm_lang_id language);
 
 		// get a string by the given key
-		// returns empty string if not found
+		// - returns empty string if not found
 		lm_val get(lm_key key) const;
 
 		// get a string by the given key
-		// returns empty string when not found
+		// - returns empty string when not found
 		lm_val operator[] (lm_key key) const;
 
 		// set the listeners array and set to update
-		void setlisteners(lm_listener_array &listeners);
+		void setlisteners(lm_listener_array& listeners);
+
+		// add a new listener to the list
+		void addlistener(multilingual::Listener listener);
 
 		// force all listeners to be updated
-		// returns false if listeners are disabled
+		// - returns false if listeners are disabled
 		bool update_listeners();
 
+		// enable listeners
+		// - if listeners were already enabled, returns false
+		bool enable_listeners();
+
 		// disable update listeners
-		// returns false if listeners were not enabled to start with
+		// - returns false if listeners were not enabled to start with
 		bool disable_listeners();
 
 		// whether or not the fallback language had to be used
 		bool fellback() const;
 	};
+
+	namespace util {
+		inline lm_str& str_trim(std::string& s) {
+			// trim right
+			s.erase(s.find_last_not_of(" ") + 1);
+			// trim left
+			s.erase(0, s.find_first_not_of(" "));
+			return s;
+		};
+	};
 };
 
-using namespace multilingual;
+// UNDEF THIS AT THE END!!!
+#define MTrans multilingual::Translator
 
-unsigned int Translator::load() {
+unsigned int MTrans::load() {
+	_fellback = false; // if this isn't the first time loading a language...
+	if (!_strvals.empty())
+		_strvals.clear();
 	std::ifstream infile(_dir + _lang + ".lang");
 	if (!infile) {
+		if (_lang == _fallback)
+			throw std::runtime_error(_dir + _lang + ".lang could not be loaded.\n");
+
 		infile = std::ifstream(_dir + _fallback + ".lang");
 		if (!infile) {
 			throw std::runtime_error("Neither " + _dir + _lang + ".lang nor " + _dir + _fallback + ".lang could be found.");
@@ -90,7 +131,7 @@ unsigned int Translator::load() {
 
 	// load from the file
 	unsigned int count = 0;
-	std::string line, temp;
+	std::string line, nkey, nval;
 	std::stringstream ss;
 	while (std::getline(infile, line)) {
 		bool valid = false;
@@ -102,38 +143,93 @@ unsigned int Translator::load() {
 		}
 		if (!valid) continue;
 
-		// process the line
+		// grab the key
 		ss = std::stringstream(line);
-		std::getline(ss, temp, '\n');
+		std::getline(ss, nkey, '=');
+
+		// trim whitespace from the start and end of the key
+		// whitespace inside the key is not recommended
+		// it will be replaced with underscores
+		multilingual::util::str_trim(nkey);
+		for (std::size_t i = 0; i < nkey.length(); i++) {
+			if (nkey[i] == ' ')
+				nkey[i] = '_';
+		}
+
+		if (nkey.empty())
+			continue;
+
+		// grab the value
+		std::getline(ss, nval, '\n');
+		
+		multilingual::util::str_trim(nval);
+
+		_strvals[nkey] = nval;
+
+		nkey.clear();
+		nval.clear();
+
+		count++;
 	}
+
+	if (_updatelisteners)
+		update_listeners();
+
+	return count;
 };
 
-void Translator::setlang(lm_lang_id language) {
+void MTrans::setdir(lm_dir newdir) {
+	_dir = newdir;
+};
+
+void MTrans::setfallback(lm_lang_id newlang) {
+	_fallback = newlang;
+};
+
+void MTrans::setlang(lm_lang_id language) {
 	_lang = language;
 };
 
-lm_val Translator::get(lm_key key) const {
-	return _strvals[key];
+lm_val MTrans::get(lm_key key) const {
+	try {
+		return _strvals.at(key);
+	} catch (const std::out_of_range& e) {
+		return key;
+	}
 };
 
-lm_val Translator::operator[] (lm_key key) const {
+lm_val MTrans::operator[] (lm_key key) const {
 	return get(key);
 };
 
-void Translator::setlisteners(lm_listener_array &listeners) {
-	_listeners(listeners);
+void MTrans::setlisteners(lm_listener_array& listeners) {
+	_listeners = lm_listener_array(listeners);
 	_updatelisteners = true;
 };
 
-bool Translator::update_listeners() {
+void MTrans::addlistener(multilingual::Listener listener) {
+	_listeners.push_back(listener);
+};
+
+bool MTrans::update_listeners() {
 	if (!_updatelisteners) return false;
 
-	// TODO
+	for (auto& l : _listeners) {
+		l.str = get(l.key);
+	}
 
 	return true;
 };
 
-bool Translator::disable_listeners() {
+bool MTrans::enable_listeners() {
+	if (_updatelisteners) return false;
+
+	_updatelisteners = true;
+
+	return true;
+};
+
+bool MTrans::disable_listeners() {
 	if (!_updatelisteners) return false;
 	
 	_updatelisteners = false;
@@ -141,8 +237,10 @@ bool Translator::disable_listeners() {
 	return true;
 };
 
-bool Translator::fellback() {
+bool MTrans::fellback() const {
 	return _fellback;
 };
+
+#undef MTrans
 
 #endif
---
+++ test/main.cpp
@@ -0,0 +1,62 @@
+#include "../src/multilingual.h"
+#include <iostream>
+#include <string>
+
+void writekeys(multilingual::Translator& t) {
+	t.load();
+	std::cout << "Fell back? " << (t.fellback() ? "Yes.\n" : "No.\n");
+	std::cout << t.get("user.name") << '\n';
+	std::cout << t.get("user.age") << '\n';
+	std::cout << t["user.height"] << '\n';
+	std::cout << t.get("gb_key") << '\n';
+}
+
+int main(void) {
+	std::cout << "Loading en_US.lang\n";
+	multilingual::Translator t("en_US");
+	writekeys(t);
+	std::cout << "Loading lang/en_GB.lang\n";
+	t.setdir("lang/");
+	t.setlang("en_GB");
+	writekeys(t);
+	std::cout << "Loading dummy file...\n";
+	t.setfallback("en_GB");
+	t.setlang("en_NOWHERE");
+	writekeys(t);
+
+
+	std::cout << "Testing listeners.\n";
+	t.setdir("./");
+	t.setlang("en_US");
+	t.setfallback("en_US");
+
+	std::string username = "";
+	std::string userage = "";
+	std::string userheight = "";
+	std::string gbkey = "";
+
+	lm_listener_array larr = {
+		{ username, "user.name" },
+		{ userage, "user.age" },
+		{ userheight, "user.height" },
+	};
+
+	t.setlisteners(larr);
+
+	t.addlistener({ gbkey, "gb_key" });
+
+	t.enable_listeners();
+
+	t.load();
+
+	std::cout << username << '\n' << userage << '\n' << userheight << '\n' << gbkey << '\n';
+
+	t.setdir("lang/");
+	t.setlang("en_GB");
+
+	t.load();
+
+	std::cout << username << '\n' << userage << '\n' << userheight << '\n' << gbkey << '\n';
+
+	return 0;
+}
